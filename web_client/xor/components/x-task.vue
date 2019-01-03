4a4f599a687b73d1b9858c0359817614e5fb042f
nagaminenot <nagamine@slogan.jp>
2019-01-03 11:55:12 +0900

    taskとcalendar間でDDできるようにした

--- web_client/xor/components/x-task.vue
+++ web_client/xor/components/x-task.vue
@@ -8,7 +8,15 @@
       <v-card-title>
         <div>
           <h3 class="headline mb-0">Task</h3>
-          <div>Task Component</div>
+          <draggable
+            v-for="list in tasks"
+            :options="{ group: 'tasks' }"
+            :key="list.id"
+            element="ul">
+            <li
+              v-for="item in list"
+              :key="item.id">{{ item }}</li>
+          </draggable>
         </div>
       </v-card-title>
     </v-card>
@@ -16,7 +24,20 @@
 </template>
 
 <script>
+import draggable from 'vuedraggable'
 export default {
-  name: 'XTask'
+  name: 'XTask',
+  components: {
+    draggable
+  },
+  data() {
+    return {
+      tasks: [
+        ['task1-1', 'task1-2', 'task1-3', 'task1-4'],
+        ['task2-1', 'task2-2', 'task2-3', 'task2-4'],
+        ['task3-1', 'task3-2', 'task3-3', 'task3-4']
+      ]
+    }
+  }
 }
 </script>
